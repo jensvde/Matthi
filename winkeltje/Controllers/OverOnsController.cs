99257193511241c381ff7e95d795973bbe900fef
jensvde <jens.vandeneynde@telenet.be>
2020-09-29 21:21:05 +0200

    final

--- winkeltje/Controllers/OverOnsController.cs
+++ winkeltje/Controllers/OverOnsController.cs
@@ -1,3 +1,4 @@
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 
 namespace winkeltje.Controllers
@@ -5,6 +6,7 @@ namespace winkeltje.Controllers
     public class OverOnsController : Controller
     {
         // GET
+        [Authorize]
         public IActionResult Index()
         {
             return View();
--- winkeltje/Startup.cs
+++ winkeltje/Startup.cs
@@ -1,15 +1,20 @@
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Text;
 using System.Threading.Tasks;
 using DAL.EF;
+using Domain;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.HttpsPolicy;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using Microsoft.IdentityModel.Tokens;
 
 namespace winkeltje
 {
@@ -30,6 +35,28 @@ namespace winkeltje
                 options.SerializerSettings.ReferenceLoopHandling = Newtonsoft.Json.ReferenceLoopHandling.Ignore
             );
             
+            // Adding Authentication  
+            services.AddAuthentication(options =>  
+                {  
+                    options.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;  
+                    options.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;  
+                    options.DefaultScheme = JwtBearerDefaults.AuthenticationScheme;  
+                })  
+  
+                // Adding Jwt Bearer  
+                .AddJwtBearer(options =>  
+                {  
+                    options.SaveToken = true;  
+                    options.RequireHttpsMetadata = false;  
+                    options.TokenValidationParameters = new TokenValidationParameters()  
+                    {  
+                        ValidateIssuer = true,  
+                        ValidateAudience = true,  
+                        ValidAudience = Configuration["JWT:ValidAudience"],  
+                        ValidIssuer = Configuration["JWT:ValidIssuer"],  
+                        IssuerSigningKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(Configuration["JWT:Secret"]))  
+                    };  
+                });  
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
